14e12db2bf418c70ce658fa4b3673bcdde46e52f
Mohanapriya <Priyarajkumar3003@gmail.com>
2026-06-09 13:52:35 +0530

    First Java Program

---
+++ src/com/test/demo.java
@@ -0,0 +1,23 @@
+package com.test;
+
+public class demo {
+    static void main(String[] args) {
+
+       //reverse
+        int  original =12345;
+        int result = 0;
+        System.out.println("original String :"+ original);
+        while(original>0){
+         int rem  =original%10;
+         result = (result * 10) + rem;
+
+            original = original /10;
+
+
+        }
+
+        System.out.println("Reverse String :"+ result);
+
+    }
+
+}
